1f287e24f98bb01e8402156747ae2e2984983092
Jake TyCyn <jaketycyn@gmail.com>
2022-03-05 09:49:47 -0800

    Refactor: authController.js - created error folder

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,18 @@ import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
 
 const registerUser = async (req, res, next) => {
   try {
-    const user = await User.create(req.body);
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      throw new BadRequestError("Please provide all values");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      throw new BadRequestError("Email already in use");
+    }
+
+    const user = await User.create({ name, email, password });
     res.status(StatusCodes.CREATED).json({ user });
   } catch (error) {
     next(error);
