4a7fe26cd1b8558c35f7fb167c927af0a998b7be
Jake TyCyn <jaketycyn@gmail.com>
2022-03-08 12:18:39 -0800

    Added: JWT Token + Lifetime

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,10 @@ const registerUser = async (req, res, next) => {
     }
 
     const user = await User.create({ name, email, password });
-    res.status(StatusCodes.CREATED).json({ user });
+
+    const token = user.createJWT();
+
+    res.status(StatusCodes.CREATED).json({ user, token });
   } catch (error) {
     next(error);
   }
